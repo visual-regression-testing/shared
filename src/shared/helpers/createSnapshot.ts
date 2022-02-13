3ec0758426961eda45953c85beceb34fc126b479
Mike DiDomizio <mikedidomizio@gmail.com>
2022-02-12 23:47:46 -0500

    Update create snapshot to not double stringify

--- src/shared/helpers/createSnapshot.ts
+++ src/shared/helpers/createSnapshot.ts
@@ -1,8 +1,8 @@
 import {InsertResponse, query} from "../../config/db";
 
-export async function createSnapshot(metadata: Object): Promise<InsertResponse> {
+export async function createSnapshot(metadata: string): Promise<InsertResponse> {
     return query(
         `INSERT INTO snapshots (metadata) VALUES (?)`,
-        [JSON.stringify(metadata)]
+        [metadata]
     )
 }
