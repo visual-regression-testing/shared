65c17c6f4e6c12be47f68dac13009e2af143671c
Mike DiDomizio <mikedidomizio@gmail.com>
2022-07-03 12:36:05 -0400

    Build creation includes branch

--- src/shared/helpers/createBuildByPercyToken.ts
+++ src/shared/helpers/createBuildByPercyToken.ts
@@ -1,7 +1,7 @@
 import {query} from "../../config/db";
 
 
-export async function createBuildByPercyToken(percyToken: string): Promise<any> {
+export async function createBuildByPercyToken(percyToken: string, branch: string, targetBranch: string | null): Promise<any> {
     let projectId: [{ id: number | null }] = [{ id: null }]
     try {
         projectId = await query<[{ id: number }]>(
@@ -14,11 +14,10 @@ export async function createBuildByPercyToken(percyToken: string): Promise<any> 
     if (projectId[0].id) {
         try {
             await query<any>(
-                `INSERT into builds (project_id) VALUES (?)`,
-                [projectId[0].id as number]
+                `INSERT into builds (project_id, baseline_branch, branch) VALUES (?, ?, ?)`,
+                [projectId[0].id, targetBranch as string, branch]
             )
 
-            // todo not tested
             return query<[{  id: number }]>(`SELECT LAST_INSERT_ID() as id`);
         } catch(e) {
             // todo
